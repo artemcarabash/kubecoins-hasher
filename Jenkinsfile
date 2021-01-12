f5b30f83fc84a10d3e11dfcb20c94b1ac3ba1244
Artiom Carabas <artemcarabash@gmail.com>
2021-01-12 18:58:30 +0100

    add comments

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,10 @@
+// Configdatei für Continuous Integration Server - Jenkins
 kubernetesPipeline {
+    // Source Git Repository die überwacht nach Push Events wird
     scmUrl = 'https://gitlab.informatik.hs-furtwangen.de/cnc/hasher-carabasa.git'
+    // Pfad zum Container der aktualisiert werden sollte
     containerImage = 'docker.informatik.hs-furtwangen.de/cnc-carabasa/hasher'
+    // Lokale Pfad zum Manifestdatei
     chartPath = './src/charts/hasher'
     releaseName = 'hasher-carabasa'
     stagingNamespace = 'cnc'
--- src/main/java/de/hfu/cnc/hasher/Hasher.java
+++ src/main/java/de/hfu/cnc/hasher/Hasher.java
@@ -13,24 +13,29 @@ import java.security.NoSuchAlgorithmException;
 
 @RestController
 public class Hasher {
-
+    // Eigene Prometheus Metrik  die mit z.B. Graphana abgerufen werden kann
     Counter requestCounter;
     MessageDigest md;
     private static final Logger LOG = LoggerFactory.getLogger(HasherApplication.class);
 
+    // Konstruktor
     public Hasher(MeterRegistry registry) throws NoSuchAlgorithmException {
         md = MessageDigest.getInstance("SHA-256");
+        // Initialisierung von Metrik
         this.requestCounter = registry.counter("hasher_requests");
     }
 
+    // Eine HTTP Post Methode die bekommt random Bytes von Randomizer Service, erstellt ein Hashwert und gibt es mit dem Response zurück
     @PostMapping("/")
     public String hash(@RequestBody byte[] data) {
         byte[] encodedHash = md.digest(data);
         LOG.info("Hasher ---- Received Request");
+        // Methode die wird mit jeder Anfrage unsere Metrik hochzählen
         requestCounter.increment();
         return bytesToHex(encodedHash);
     }
 
+    // Methode um eine Hex Wert zu generieren
     private String bytesToHex(byte[] hash) {
         StringBuffer hexString = new StringBuffer();
         for (int i = 0; i < hash.length; i++) {
